95eb3d2a96b26095202f3ae9fc19a96fbf737555
davzu_90 <davzu_90@hotmail.com>
2017-01-27 21:52:55 -0500

    clase 05 fin

--- Clase05/js-carrusel.js
+++ Clase05/js-carrusel.js
@@ -1,1 +1,36 @@
 var lista = ["izq-0","izq-1","izq-2","centrada","der-2","der-1","der-0"];
+
+var quitar = function() {
+	var elementos = document.querySelectorAll("img");
+	var longitud = elementos.length;
+	/*for (var i = 0; i < longitud; i++) {
+		//elementos[i].classList.remove("izq-0","izq-1","izq-2","centrada","der-2","der-1","der-0");
+		/*for(var j = 0; j < lista.length; j++) {
+			elementos[i].classList.remove(lista[j]);
+		}
+	}*/
+	elementos.forEach(function(el, ind){
+		el.classList.remove("izq-0", "izq-1", "izq-2", "centrada", "der-2", "der-1", "der-0");
+	});
+}
+
+var asignar = function() {
+	var elementos = document.querySelectorAll("img");
+	var longitud = elementos.length;
+	/*for (var i = 0; i < longitud; i++) {
+		elementos[i].classList.add(lista[i]);
+	}*/
+	elementos.forEach(function(el, ind){
+		el.classList.add(lista[ind]);
+	});
+}
+
+var cambiar = function() {
+	var primerElemento = lista.shift();
+	lista.push(primerElemento);
+	console.log(lista);
+	quitar();
+	asignar();
+}
+
+setInterval(cambiar, 3000)
